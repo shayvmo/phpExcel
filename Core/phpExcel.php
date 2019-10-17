90cf4d83b46000801e1eacb487244999b6404063
Eric <1006897172@qq.com>
2019-10-17 14:16:34 +0800

    数据格式兼容不填列名

--- Core/phpExcel.php
+++ Core/phpExcel.php
@@ -37,6 +37,12 @@ class phpExcel
     public $Keywords;
     public $Category;
     public $data;
+    public $index_key = [
+        'A','B','C','D','E','F','G','H','I','J',
+        'K','L','M','N','O','P','Q','R','S','T',
+        'U','V','W','X','Y','Z',
+
+    ];
 
     public function __construct($data)
     {
@@ -144,8 +150,11 @@ class phpExcel
             if (!empty($this->data['data'])) {
                 foreach ($this->data['data'] as $key => $value) {
                     foreach ($value as $k => $v) {
-                        $activeSheet->setCellValueExplicit($k . ($key+1), $v,DataType::TYPE_STRING);
-
+                        if(in_array($k,$this->index_key)) {
+                            $activeSheet->setCellValueExplicit($k . ($key+1), $v,DataType::TYPE_STRING);
+                        } elseif(isset($this->index_key[$k])) {
+                            $activeSheet->setCellValueExplicit($this->index_key[$k] . ($key+1), $v,DataType::TYPE_STRING);
+                        }
                     }
                 }
             }
